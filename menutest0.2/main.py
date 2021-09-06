4ea873d07b55505933c4b9475138a3c4729c8189
KanyoHUN <57896414+KanyoHUN@users.noreply.github.com>
2021-09-06 20:01:01 +0200

    Add files via upload

--- menutest0.2/main.py
+++ menutest0.2/main.py
@@ -3,19 +3,28 @@ import certifi
 import requests
 import mysql.connector
 from kivy.app import App
+from kivy.graphics import Color, Line
 from kivy.metrics import dp
-from kivy.properties import StringProperty, BooleanProperty
+from kivy.properties import StringProperty, BooleanProperty, ObjectProperty
 from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.spinner import Spinner
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.videoplayer import VideoPlayer
 from functools import partial
+
+from kivy.uix.widget import Widget
 from pytube import YouTube
 
 os.environ['SSL_CERT_FILE'] = certifi.where()
 
 video_dict = {}
 previous_screen = 'menu'
+workouts = []
+workout_buttons = []
 
 
 class ScreenManager1(ScreenManager):
@@ -59,6 +68,11 @@ class MenuScreen(Screen):
         for row in myresult:
             video_dict[row[1]] = row[0]
             print("Link loaded")
+
+    def on_workout_editor_button_press(self):
+        global previous_screen
+        self.manager.current = 'editor'
+        previous_screen = 'editor'
 
 class LinkListScreen(Screen):
 
@@ -156,6 +170,167 @@ class VideoScreen(Screen):
     def delete_previous_video(self):
         if os.path.isfile(self.removable):
             os.remove(self.removable)
+        else:
+            pass
+
+
+class WorkoutEditor(Screen):
+
+    class Display1(FloatLayout):
+        vertical_lines = []
+        horizontal_lines = []
+
+        def __init__(self, **kwargs):
+            super().__init__(**kwargs)
+            self.init_vertical_lines()
+            self.init_horizontal_lines()
+
+        def on_size(self, *args):
+            self.update_vertical_lines()
+            self.update_horizontal_lines()
+
+        def init_vertical_lines(self):
+            with self.canvas.before:
+                Color(1, 1, 1)
+                for i in range(0, 8):
+                    self.vertical_lines.append(Line())
+
+        def update_vertical_lines(self):
+            for i in range(0, 8):
+                x1, y1 = 0+i/7*self.width, .2 * self.height
+                x2, y2 = 0+i/7*self.width, self.height
+                self.vertical_lines[i].points = [x1, y1, x2, y2]
+
+        def init_horizontal_lines(self):
+            with self.canvas.before:
+                Color(1, 1, 1)
+                for i in range(0, 25):
+                    self.horizontal_lines.append(Line())
+
+        def update_horizontal_lines(self):
+            for i in range(0, 25):
+                x1, y1 = 0, .2*self.height + i/30*self.height
+                x2, y2 = self.width, .2*self.height + i/30*self.height
+                self.horizontal_lines[i].points = [x1, y1, x2, y2]
+
+        def on_back_button_press(self):
+            global previous_screen
+            self.parent.manager.current = 'menu'
+            previous_screen = 'menu'
+
+        def on_add_new_button_press(self):
+            global previous_screen
+            new_workout_screen = WorkoutTemplate(name='temp')
+            self.parent.manager.add_widget(new_workout_screen)
+            self.parent.manager.current = 'temp'
+            previous_screen = 'temp'
+
+    def add_buttons(self):
+        global workout_buttons
+        global workouts
+        for child in self.children:
+            if isinstance(child, Button) and child not in workout_buttons:
+                self.remove_widget(child)
+                del child
+            else:
+                pass
+
+        for child in self.manager.children:
+            if isinstance(child, WorkoutTemplate) and child not in workouts:
+                self.remove_widget(child)
+                del child
+
+        for button in workout_buttons:
+            if not button.parent == self:
+                self.add_widget(button)
+
+
+class WorkoutTemplate(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        global workouts
+        self.selected_types = []
+        self.workout_index = len(workouts)
+        self.workouts_length_on_create = len(workouts) + 1
+        self.screen_name = ''
+        self.label_text = 'Selected Workout types: '
+        self.start_time = 99
+        self.end_time = 99
+        self.workout_name = ''
+
+    def spinner_clicked(self, value):
+        if value not in self.selected_types:
+            self.selected_types.append(value)
+            self.label_text += value+', '
+
+        self.ids.workout_types_selected.text = self.label_text
+
+    def on_save_workout_button_press(self):
+        global workouts
+        global workout_buttons
+        global previous_screen
+        time_difference = self.end_time - self.start_time
+        if self not in workouts:
+            workouts.append(self)
+        self.workout_name = 'Workout ' + str(len(workouts))
+        button = Button(text=self.workout_name, size_hint=(1/7, (0.8/24)*time_difference), pos_hint={"center_x": 1/7/2, "top":(1-(.8/24)*self.start_time)})
+        button.on_press = self.on_workout_button_click
+        workout_buttons.append(button)
+
+        self.manager.current = 'editor'
+        previous_screen = 'editor'
+
+    def on_delete_workout_button_press(self):
+        global workout_buttons
+        global workouts
+        global previous_screen
+
+        if self.workouts_length_on_create > len(workouts):
+            diff = self.workouts_length_on_create - len(workouts)
+            self.workout_index -= diff
+
+        workouts.pop(self.workout_index)
+        workout_buttons.pop(self.workout_index)
+
+        self.manager.current = 'editor'
+        previous_screen = 'editor'
+
+    def on_temp_leave(self):
+        global workouts
+        if self.name == 'temp':
+            self.name = self.workout_name
+        else:
+            pass
+
+    def on_workout_button_click(self):
+        global previous_screen
+        self.manager.current = self.name
+        previous_screen = self.name
+
+    def on_remove_last_button_press(self):
+        if len(self.selected_types) > 0:
+            removable = self.selected_types.pop()
+            print(removable)
+            text_len = len(self.label_text)
+            for i in range(text_len, text_len-(len(removable)+2), -1):
+                length = len(self.label_text)
+                self.label_text = self.label_text[:length-1]
+            self.ids.workout_types_selected.text = self.label_text
+
+    def starting_time_selected(self, value):
+        self.ids.end_time_spinner.text = 'End time(by hour)'
+        self.ids.save_button.disabled = True
+        self.start_time = int(value)
+        end_time_values = []
+        for i in range(self.start_time+1, 25):
+            end_time_values.append(str(i))
+        self.ids.end_time_spinner.values = end_time_values
+        self.ids.end_time_spinner.disabled = False
+
+    def end_time_selected(self, value):
+        if not value == 'End time(by hour)':
+            self.end_time = int(value)
+            self.ids.save_button.disabled = False
         else:
             pass
 

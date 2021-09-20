1ec2d05483a8d5027bf1303c5d19e519e6fbbd5e
KanyoHUN <gonczy.csaba12@gmail.com>
2021-09-20 10:48:53 +0200

    Changed button text in kv and modified example_video_screen.py
    removed create_display function, added on_leave function overwrite to fix memory leake, added pre_enter function to rename back button

--- menutest0.2/example_video_screen.py
+++ menutest0.2/example_video_screen.py
@@ -1,22 +1,15 @@
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.videoplayer import VideoPlayer
 import config
 from video_list import VideoScreen
 
 
 class ExampleVideo(VideoScreen):
 
-    def create_display(self):
-        for child in self.children:
-            if isinstance(child, BoxLayout):
-                box = child
-                video = VideoPlayer(id='video', allow_stretch=True)
-                button = Button(id='back_button', text='Back to Editor', size_hint=(1, .2), disabled=True)
-                # Customizable button above. Do not change id,text or disabled !!!!
-                button.on_press = self.on_back_to_videos_button_press  # Do not change this line!
-                box.add_widget(video)
-                box.add_widget(button)
+    def on_pre_enter(self, *args):
+        self.ids.back_button.text = 'Back To Editor'
+
+    def on_leave(self, *args):
+        self.manager.remove_widget(self)
+        del self
 
     def on_back_to_videos_button_press(self):
         self.manager.current = config.previous_screen
@@ -24,4 +17,3 @@ class ExampleVideo(VideoScreen):
         self.ids.video.source = ''
         self.ids.video.state = 'stop'
         self.delete_previous_video()
-        self.ids.back_button.disabled = True
